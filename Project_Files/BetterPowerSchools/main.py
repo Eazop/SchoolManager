8eae0f66f3d250bc95ed555e669503b1b5cd65f7
Alec <ealec95@gmail.com>
2018-04-10 14:34:53 -0400

    Mergred BPSObjects into main.py. Home page now displays a list of courses and a link to them

--- Project_Files/BetterPowerSchools/main.py
+++ Project_Files/BetterPowerSchools/main.py
@@ -2,10 +2,107 @@ from flask import Flask, request, session, g, redirect, url_for, abort, \
  render_template, flash
 
 
-import BPSObjects
-
 from jinja2 import Template
 import pymysql
+
+import pymysql
+
+class Course:
+    courseID = ""
+    teacherID = ""
+    subject = ""
+    days = ""
+    time = ""
+
+    def init(self, courseID, teacherID, subject, days, time):
+        self.courseID = courseID
+        self.teacherID = teacherID
+        self.subject = subject
+        self.days = days
+        self.time = time
+
+
+
+class Assignment:
+    dueDate = None
+    courseID = None
+    studentID = None
+    assignmentID = None
+    description = None
+    grade = None
+
+    def init(self, assignID):
+        self.assignmentID = assignID
+        db = pymysql.connect(host='104.196.175.51', user='BPS', password='betterpowerschools', db='better_power_schools')
+        cur = db.cursor()
+        q = "SELECT * FROM assignments WHERE assignmentID = " + str(self.assignmentID)
+        cur.execute(q)
+        l = cur.fetchall()
+        self.studentID = l[1]
+        self.description =l[2]
+        self.courseID = l[3]
+        self.dueDate = l[4]
+        if(l[5] != 'null'):
+            self.grade = l[5]
+    def init(self, assignID, studID, Desc, CourID, Due, Gra):
+        self.assignmentID = assignID
+
+        self.studentID = studID
+        self.description = Desc
+        self.courseID = CourID
+        self.dueDate = Due
+        if(Gra != 'null'):
+            self.grade = Gra
+
+class Teacher:
+    teacherID = 0
+    currentCourses = []
+    def init(self,teachID):
+        self.teacherID = teachID
+        db = pymysql.connect(host='104.196.175.51', user='BPS', password='betterpowerschools', db='better_power_schools')
+        cur = db.cursor()
+        q ="Select * from courses where TeacherID =" + str(self.teacherID)
+        cur.execute(q)
+        temp = cur.fetchall()
+        for course in temp:
+            c = Course()
+            c.init(course[0], course[1], course[2], course[3], course[4])
+            self.currentCourses.append(c)
+        db.close()
+
+#Adds a new assignment to the data base
+    def submitAssignment(self, courseID, Description, DueDate):
+        studentHolder = []
+        db = pymysql.connect(host='104.196.175.51', user='BPS', password='betterpowerschools', db='better_power_schools')
+        cur = db.cursor()
+        for x in range(1, 7):
+            cur.execute('SELECT * FROM students WHERE Course' +str(x)+' = ' + str(courseID))
+            l = cur.fetchall()
+            for student in l:
+                studentHolder.append(student[0])
+
+        for student in studentHolder:
+            q = 'INSERT INTO assignments (StudentID, Description, CourseID, DueDate) VALUES (' + str(student) + ', \"' + Description + '\", ' + str(courseID) + ', \"' + DueDate + '\")'
+            cur.execute(q)
+        db.commit()
+        db.close()
+
+#Returns a list of the assignment objects associated with the teacher
+    def getAssignments(self):
+        db = pymysql.connect(host='104.196.175.51', user='BPS', password='betterpowerschools', db='better_power_schools')
+        cur = db.cursor()
+        assignmentHolder = []
+        for course in self.currentCourses:
+            q = "SELECT * FROM assignments WHERE courseID = " + str(course)
+            cur.execute(q)
+            l = cur.fetchall()
+            a = Assignment()
+            a.init(l[0][0], l[0][1], l[0][2], l[0][3], l[0][4], l[0][5])
+            assignmentHolder.append(a)
+        return assignmentHolder
+
+
+
 
 app = Flask(__name__)
 
@@ -22,10 +119,8 @@ def connect_db():
 
 @app.route('/', methods=['GET','POST'])
 def home():
-        t.init(int(session['userid']))
-        print(str(t.currentCourses[0]) + " " + t.currentCourses[1])
-        course = t.currentCourses
-        return render_template('home.html', courses = course)
+        courses = t.currentCourses
+        return render_template('home.html', courses = courses)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -84,7 +179,6 @@ def login():
 def loggedin():
     error = None
     t.init(session['userid'])
-    print(session['userid'])
     return render_template('loggedin.html', error = error)
 
 
@@ -110,7 +204,7 @@ def assignmentList():
 	return render_template('Assignments.html', assignmentList = t.getAssignments())
 
 global t
-t = BPSObjects.Teacher()
+t = Teacher()
 
 if __name__ == '__main__':
 	app.run(host='127.0.0.1', port=8080, debug=True)

460877232c92d92f13c1e9580f2bfa7380f4465f
pereze9 <36862555+pereze9@users.noreply.github.com>
2018-04-25 11:14:15 -0400

    Add files via upload
    added simple line to fix logout issue and added stuff for parent home screen use

---
+++ BPS.py
@@ -0,0 +1,233 @@
+import pymysql
+
+#The student class holds all the values for a student
+#This should only be initialized if the user is logging in as a student successfully
+
+class Student:
+    firstName = None
+    lastName = None
+    studentID = None
+    parentID = None
+    courses = []
+
+    #Initializes the class using just a studentID to pull the rest of the data from the database
+    def init(self, studentID):
+        q = Query("SELECT * FROM students WHERE studentID = " + str(studentID))
+        self.studentID = q[0][0]
+        self.firstName = q[0][1]
+        self.lastName = q[0][2]
+        self.parentID = q[0][3]
+        for x in range(4, 10):
+            if q[0][x] :
+                c = Course()
+                c.init(q[0][x])
+                self.courses.append(c)
+
+    def getParent(self):
+        return parentID
+
+    #Posts a new message in the database
+    def sendMessage(self, message, teacherID):
+        now = datetime.datetime.now()
+        q = "INSERT INTO messages (StudentID, TeacherID, Message, Time) VALUES ("
+        q += str(self.studentID) + ", " + str(teacherID) + ", " + message + ", " + now.strftime("%Y-%m-%d %H:%M") + ")"
+        m = Query(q)
+
+    #Sets all values back to their default (this should be used for logging out)
+    def deconstruct(self):
+        self.firstName = None
+        self.lastName = None
+        self.studentID = None
+        self.parentID = None
+        self.courses = []
+        
+class Parent:
+
+    
+    studentID = None    
+    parentID = None
+    courses = []
+    
+    teacherID = 0
+    currentCourses = []
+    def init(self,parentID):
+        self.parentID = parentID
+  
+   
+
+
+    def getStudent(self):
+        return studentID
+
+  
+
+    #Sets all values back to their default (this should be used for logging out)
+    def deconstruct(self):
+        self.firstName = None
+        self.lastName = None
+        self.studentID = None
+        self.parentID = None
+        self.courses = []
+class Course:
+    courseID = ""
+    teacherID = ""
+    subject = ""
+    days = ""
+    time = ""
+
+    #Initializes the course using a set of given values
+    def init(self, courseID, teacherID, subject, days, time):
+        self.courseID = courseID
+        self.teacherID = teacherID
+        self.subject = subject
+        self.days = days
+        self.time = time
+
+    #initialzes the course using just a course ID to pull the rest of the information from the database
+    def init(self, courseID):
+        q = Query("SELECT * from courses where courseID = " + str(courseID))
+        print(q)
+        self.courseID = courseID
+        self.teacherID = q[0][1]
+        self.subject = q[0][2]
+        self.days = q[0][3]
+        self.time = q[0][4]
+
+    #Returns a list of assignments associated with the particular course
+    def getAssignments(self):
+        return Query("SELECT DISTINCT Description, DueDate FROM assignments WHERE CourseID =" + str(self.courseID()))
+
+    #Sets all values of the course back to their default VALUES
+    #This should be used whenever the user is done with it
+    def deconstruct(self):
+        self.courseID = None
+        self.teacherID = None
+        self.subject = None
+        self.days = None
+        self.time = None
+
+
+class Assignment:
+    title = None
+    dueDate = None
+    courseID = None
+    studentID = None
+    assignmentID = None
+    description = None
+    grade = None
+
+#This initializes the assignment using just an assignmentID by pulling the rest
+# of the values from the database.
+    def init(self, assignID):
+        self.assignmentID = assignID
+        db = pymysql.connect(host='104.196.175.51', user='BPS', password='betterpowerschools', db='better_power_schools')
+        cur = db.cursor()
+        q = "SELECT * FROM assignments WHERE assignmentID = " + str(self.assignmentID)
+        cur.execute(q)
+        l = cur.fetchall()
+        self.studentID = l[1]
+        self.description =l[2]
+        self.courseID = l[3]
+        self.dueDate = l[4]
+        if(l[5] != 'null'):
+            self.grade = l[5]
+        self.title = l[6]
+
+#This initializes the assignment using given variables. This should only be used
+# when the assignment is being created and added to the database
+    def init(self, assignID, studID, Desc, CourID, Due, Gra, Title):
+        self.assignmentID = assignID
+
+        self.studentID = studID
+        self.description = Desc
+        self.courseID = CourID
+        self.dueDate = Due
+        if(Gra != 'null'):
+            self.grade = Gra
+        self.title = Title
+
+    def deconstruct(self):
+        self.title = None
+        self.assignmentID = None
+        self.studentID = None
+        self.description = None
+        self.dueDate = None
+        self.grade = None
+
+#The teacher class holds all of the values of a teacher. It should match up
+# exactly with the values that are in the database
+
+class Teacher:
+    teacherID = 0
+    currentCourses = []
+    def init(self,teachID):
+        self.teacherID = teachID
+        q ="Select CourseID from courses where TeacherID =" + str(self.teacherID)
+        temp = Query(q)
+        for course in temp:
+            c = Course()
+            c.init(course[0])
+            self.currentCourses.append(c)
+
+#Adds a new assignment to the data base
+    def submitAssignment(self, courseID, title, Description, DueDate):
+        studentHolder = []
+        db = pymysql.connect(host='104.196.175.51', user='BPS', password='betterpowerschools', db='better_power_schools')
+        cur = db.cursor()
+        for x in range(1, 7):
+            cur.execute('SELECT * FROM students WHERE Course' +str(x)+' = ' + str(courseID))
+            l = cur.fetchall()
+            for student in l:
+                studentHolder.append(student[0])
+
+        for student in studentHolder:
+            q = 'INSERT INTO assignments (StudentID, Description, CourseID, DueDate, Title) VALUES (' + str(student) + ', \"' + Description + '\", ' + str(courseID) + ', \"' + DueDate + '\", \"' + title + '\")'
+            cur.execute(q)
+        db.commit()
+        db.close()
+
+#Returns a list of the assignment objects associated with the teacher
+    def getAssignments(self):
+        db = pymysql.connect(host='104.196.175.51', user='BPS', password='betterpowerschools', db='better_power_schools')
+        cur = db.cursor()
+        assignmentHolder = []
+        for course in self.currentCourses:
+            q = "SELECT * FROM assignments WHERE CourseID = " + str(course.courseID)
+            cur.execute(q)
+            l = cur.fetchall()
+            for assignment in l:
+                a = Assignment()
+                a.init(assignment[0], assignment[1], assignment[2], assignment[3], assignment[4], assignment[5])
+                assignmentHolder.append(a)
+        return assignmentHolder
+
+    def deconstruct(self):
+        self.teacherID = None
+        self.currentCourses = []
+
+
+class Message:
+    messageID = -1
+    message = None
+    studentID = None
+    teacherID = None
+    sendDate = None
+
+    # def sendMessage(self):
+    #     Query("INSERT INTO messages (StudentID, TeacherID, Message, Time) VALUES ("+ self.studentID + ", ")
+
+    def deconstruct(self):
+        self.messageID = -1
+        self.message = None
+        self.studentID = None
+        self.teacherID = None
+        self.sendDate = None
+
+
+def Query(query):
+    db = pymysql.connect(host='104.196.175.51', user='BPS', password='betterpowerschools', db='better_power_schools')
+    cur = db.cursor()
+    cur.execute(query)
+    l = cur.fetchall()
+    db.close()
+    return l
---
+++ main.py
@@ -0,0 +1,202 @@
+from flask import Flask, request, session, g, redirect, url_for, abort, \
+ render_template, flash
+import datetime
+
+
+from jinja2 import Template
+import pymysql
+import BPS
+
+def Query(query):
+    db = pymysql.connect(host='104.196.175.51', user='BPS', password='betterpowerschools', db='better_power_schools')
+    cur = db.cursor()
+    cur.execute(query)
+    l = cur.fetchall()
+    db.close()
+    return l
+
+
+app = Flask(__name__)
+
+app.config.update(dict(
+DATABASE='',
+SECRET_KEY='development key',
+USERNAME='',
+PASSWORD=''
+))
+
+def connect_db():
+	db = pymysql.connect(host='104.196.175.51', user='BPS', password='betterpowerschools', db='better_power_schools')
+	return db
+
+@app.route('/', methods=['GET','POST'])
+def home():
+        if s.studentID != None:
+            courses = s.courses
+            teachers = []
+            for course in courses:
+                q = "SELECT TeacherID, Fname, Lname FROM  teachers WHERE Course1 = " + str(course.courseID) + " OR Course2 = " + str(course.courseID) + " OR Course3 = " + str(course.courseID) + " OR Course4 = " + str(course.courseID)
+                teacher = Query(q)
+                print(teacher)
+                teachers.append(teacher)
+            return render_template('studentHome.html', courses = courses, teachers = teachers)
+        elif t.teacherID != None:
+            courses = t.currentCourses
+            return render_template('teacherHome.html', courses = courses)
+	elif p.parentID != None:
+	        
+		return render_template('parentHome.html')
+	else:
+	    return render_template('login.html')
+	
+@app.route('/schedule', methods=['GET','POST'])
+def Schedule():
+    courses = p.courses
+    teachers = []
+    for course in courses:
+	q = "SELECT TeacherID, Fname, Lname FROM  teachers WHERE Course1 = " + str(course.courseID) + " OR Course2 = " + str(course.courseID) + " OR Course3 = " + str(course.courseID) + " OR Course4 = " + str(course.courseID)
+	teacher = Query(q)
+	print(teacher)
+	teachers.append(teacher)
+    return render_template('schedule_student.html', courses = courses, teachers = teachers)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	error = None
+	db = pymysql.connect(host='104.196.175.51', user='BPS', password='betterpowerschools', db='better_power_schools')
+	if request.method == 'POST':
+		stringusername = None
+		stringpassword = None
+
+		role = request.form['role']
+
+		curs = db.cursor()
+		if (role == "teacher"):
+			ro = curs.execute('SELECT TeacherID FROM teachers WHERE TeacherID=%s', [request.form['username']])
+		elif (role == "student"):
+			ro = curs.execute('SELECT StudentID FROM students WHERE StudentID=%s', [request.form['username']])
+		elif (role == "parent"):
+		        ro = curs.execute('SELECT ParentID FROM parents WHERE ParentID=%s', [request.form['username']])
+			
+
+		USERNAME = curs.fetchone()
+		try:
+			stringusername = ''.join(str([x for x in USERNAME]))
+		except:
+			print()
+
+		curs = db.cursor()
+		if (role == "teacher"):
+			ro = curs.execute('SELECT TeacherID, Password FROM teachers WHERE TeacherID=%s and Password=%s', [request.form['username'], request.form['password']])
+		elif (role == "student"):
+			ro = curs.execute('SELECT StudentID, Password FROM students WHERE StudentID=%s and Password=%s', [request.form['username'], request.form['password']])
+
+		elif (role == "parent"):
+			ro = curs.execute('SELECT ParentID, Password FROM parents WHERE ParentID=%s and Password=%s', [request.form['username'], request.form['password']])
+
+		COMBINED = curs.fetchone()
+		try:
+			stringpassword = ''.join(str(COMBINED[1]))
+		except:
+			print()
+
+		if str(request.form['username']) != stringusername[1]:
+			error = 'Bad username'
+			#flash(error)
+		elif str(request.form['password']) != stringpassword:
+			error = 'Bad password'
+			#flash(error)
+		else:
+			session['logged_in'] = True
+			session['userid'] = request.form['username']
+			session['role'] = role
+
+
+			return redirect(url_for('loggedin'))
+	return render_template('login.html', error=error)
+
+@app.route('/loggedin', methods=['GET'])
+def loggedin():
+    error = None
+    if session['role'] == "teacher":
+        t.init(session['userid'])
+    elif session['role'] == 'student':
+        s.init(session['userid'])
+    elif session['role'] == 'parent':
+	p.init(session['userid'])
+    
+
+    return render_template('loggedin.html', error = error)
+
+
+@app.route('/loggedout', methods=['GET'])
+def loggedout():
+	error = None
+	return render_template('loggedout.html', error=error)
+
+@app.route('/logout')
+def logout():
+    t.deconstruct()
+    s.deconstruct()
+    p.deconstruct()
+    session.pop('userid', None)
+    session.pop('logged_in', None)
+    return redirect(url_for('loggedout'))
+
+#Example of creating a dynamic route. <name> is an argument
+#that is passed to the function invoked by the route
+@app.route('/name/<name>')
+def get_name(name):
+	return "Hello "+name
+
+@app.route('/Assignments')
+def assignmentList():
+	return render_template('AssignmentList.html', assignments = t.getAssignments())
+
+@app.route('/Courses/<courseNum>', methods=['GET', 'POST'])
+def assignmentCourse(courseNum):
+        a = []
+        assignments = Query("SELECT DISTINCT Title, Description, DueDate FROM assignments WHERE CourseID =" + str(courseNum))
+        for assignment in assignments:
+                temp = BPS.Assignment()
+                temp.assignmentID = assignment[0]
+                temp.description = assignment[1]
+                temp.dueDate = assignment[2]
+                a.append(temp)
+
+        return render_template('Assignments.html', assignments = a, courseNum = courseNum)
+
+@app.route('/Add', methods=['GET', 'POST'])
+def assignmentAdd():
+    courseNum = request.form['courseNum']
+
+    t.submitAssignment(int(request.form['courseNum']),request.form['Title'], request.form['Description'], request.form['DueDate'])
+
+    return redirect(url_for('assignmentCourse', courseNum = courseNum))
+
+@app.route('/Courses/<courseNum>/<assignTitle>')
+def List(courseNum, assignTitle):
+    listID =[]
+    nameList = []
+    gradeList = []
+    assignID = []
+    x=0
+    temp = Query("SELECT * FROM students WHERE Course1 =" + str(courseNum) +" OR Course2 =" + str(courseNum) +" OR Course3 =" + str(courseNum) +" OR Course4 =" + str(courseNum) +" OR Course5 =" + str(courseNum) +" OR Course6 = " + str(courseNum))
+    for t in temp:
+        listID.append(t[0])
+        nameList.append(t[2] + " " + t[1])
+        temp2 = Query("SELECT assignmentID, Grade FROM assignments WHERE StudentID =" + str(t[0]) + " AND Title = \"" + assignTitle + "\"")
+        # assignID.append(temp2[0])
+        gradeList.append(temp2[1]) if (len(temp2) == 2) else gradeList.append(None)
+        x+=1
+    # temp2 = Query("SELECT assignmentID, Grade FROM assignments WHERE CourseID =" + str(courseNum) + " AND Title = \"" + assignTitle)
+    # for t in temp:
+    #     l[0][x] = t
+    return render_template('StudentAssignments.html', studentIDs = listID, nameList = nameList, gradeList = gradeList, assignID = assignID, total = x)
+
+global t, s , p
+t = BPS.Teacher()
+s = BPS.Student()
+p = BPS.Parent()
+if __name__ == '__main__':
+	app.run(host='127.0.0.1', port=8080, debug=True)

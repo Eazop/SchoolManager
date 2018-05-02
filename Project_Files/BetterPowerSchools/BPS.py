2f2a3758c8991a3fcf79a1633fdc7a66e2d1ea29
Alec Edwards <ealec95@gmail.com>
2018-05-02 12:53:14 -0400

    Revert "Added comments throughout main.py"
    This reverts commit ebd3cb73e758ff8e2d4891ea01db4e37c4ba03ab.

--- Project_Files/BetterPowerSchools/BPS.py
+++ Project_Files/BetterPowerSchools/BPS.py
@@ -42,7 +42,9 @@ class Student:
         self.courses = []
 
 class Parent:
-    student = None
+
+
+    studentID = None
     parentID = None
     courses = []
 
@@ -50,23 +52,17 @@ class Parent:
     currentCourses = []
     def init(self,parentID):
         self.parentID = parentID
-        q = "SELECT studentID FROM students WHERE parentID =" + self.ParentID
-        q = Query(q)
-        self.student = Student()
-        self.student.init(q[0][0])
-
 
     def getStudent(self):
-        return student
+        return studentID
 
     #Sets all values back to their default (this should be used for logging out)
     def deconstruct(self):
         self.firstName = None
         self.lastName = None
-        self.student = None
+        self.studentID = None
         self.parentID = None
         self.courses = []
-
 class Course:
     courseID = ""
     teacherID = ""
